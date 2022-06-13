16ae6fd85e4fe05558a49c8777399762f97d20bb
reggiew <rwang150@asu.edu>
2022-06-13 15:35:39 -0700

    No changes

---
+++ divide.test.js
@@ -0,0 +1,7 @@
+const divide = require('./divide');
+
+test('divides 20 by 5 to equal 4', () => {
+
+    expect(divide(20, 5)).toBe(4);
+});
+
---
+++ subtract.test.js
@@ -0,0 +1,7 @@
+const subtract = require('./subtract');
+
+test('subtracts 10 from 2 to equal 8', () => {
+
+    expect(subtract(10, 2)).toBe(8);
+}
+);
